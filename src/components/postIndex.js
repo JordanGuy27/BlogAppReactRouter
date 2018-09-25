f2e65fbdb802da162531d0c11d3cd7b20891e66a
Jordan Guy <jordan.guy.277@gmail.com>
2018-09-25 12:00:10 -0400

    fetch post action created

---
+++ src/components/postIndex.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div>
+                Posts Index
+            </div>
+        );
+    }
+}
+
+export default PostsIndex;
+
+//want to change the state of the information coming back as the posts from an array to an object for easier post access
--- src/index.js
+++ src/index.js
@@ -3,27 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/postIndex';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() {return <div>Hello!</div>}
-}
-
-class Bye extends React.Component {
-  render() { return <div>Bye!</div> }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/bye" component={Bye} />
+        <Route path="/" component={PostsIndex} />
       </div> 
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
+
